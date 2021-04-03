553ba6e64e13c66985265a4ae421cb71916e2ce8
WISNU GINANJAR SAPUTRA <wisnuwiry@gmail.com>
2021-04-03 16:40:24 +0700

    Fix unit test

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:dev_to/app/app.dart';
-import 'package:dev_to/counter/counter.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(MaterialApp), findsOneWidget);
     });
   });
 }
